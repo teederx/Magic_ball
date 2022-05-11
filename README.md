93605959663601cc0f045b78a715185622d327af
Idowu Favour <idowufavour07@gmail.com>
2022-05-11 17:01:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# magic_ball
+
+A Magic ball project.
+
+This app helps to answer questions randomly :) 
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Magicball(),
+    ),
+  );
+}
+
+class Magicball extends StatelessWidget {
+  const Magicball({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Ask Me Anything'),
+      ),
+      body: MagicBall(),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ball_no = 1;
+
+  void changeBall() {
+    setState(() {
+      ball_no = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: () {
+            changeBall();
+          },
+          child: Image.asset('images/ball$ball_no.png'),
+        ),
+      ),
+    );
+  }
+}
